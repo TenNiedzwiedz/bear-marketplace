14eff4c3bcbb11270c7392e76a27b007b40f23fc
TenNiedzwiedz <krawczyk_wojciech@wp.pl>
2026-07-04 22:21:58 +0200

    feat: Add /ogloszenia listings route
    Route the listings index to ListingController.

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', HomeController::class);
+Route::get('/ogloszenia', [ListingController::class, 'index'])->name('listings.index');
 
 Route::get('/design-cheatsheet', function () {
     return Inertia::render('DesignCheatsheet');

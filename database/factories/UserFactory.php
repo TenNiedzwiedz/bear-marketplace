303652bc39446956275bc42665c8c1f30100b03b
TenNiedzwiedz <krawczyk_wojciech@wp.pl>
2026-07-04 21:53:16 +0200

    feat: Add account type states to UserFactory
    Default new users to private and add a company() state for company
    accounts.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Enums\AccountType;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
@@ -30,6 +31,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'account_type' => AccountType::Private,
         ];
     }
 
@@ -40,6 +42,17 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * A company account. Pair with CompanyProfile::factory() to attach details.
+     */
+    public function company(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'name' => fake()->company(),
+            'account_type' => AccountType::Company,
         ]);
     }
 }

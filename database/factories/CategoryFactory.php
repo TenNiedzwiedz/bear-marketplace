26de1e8d67e54454adf439c00dd12d6b2143d071
TenNiedzwiedz <krawczyk_wojciech@wp.pl>
2026-07-04 21:53:16 +0200

    feat: Add CategoryFactory
    Generate categories with unique slugs and a childOf() state for
    nesting.

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends Factory<Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $name = fake()->unique()->words(2, true);
+
+        return [
+            'name' => Str::ucfirst($name),
+            'slug' => Str::slug($name).'-'.fake()->unique()->numberBetween(1, 100000),
+            'parent_id' => null,
+            'position' => 0,
+        ];
+    }
+
+    /**
+     * Nest this category under the given parent.
+     */
+    public function childOf(Category $parent): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'parent_id' => $parent->id,
+        ]);
+    }
+}

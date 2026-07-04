f9e55b3199e89486b1677fe630df51408b157c7b
TenNiedzwiedz <krawczyk_wojciech@wp.pl>
2026-07-04 17:55:18 +0200

    feat: Render home page at /
    Point the root route at the new Home page and drop the placeholder
    Welcome render and its unused version props.

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,10 @@
 <?php
 
-use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return Inertia::render('Home');
 });
 
 Route::get('/design-cheatsheet', function () {

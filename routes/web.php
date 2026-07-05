685c5a38b949acd6c1e4869157e86a3ac797f90c
TenNiedzwiedz <krawczyk_wojciech@wp.pl>
2026-07-05 12:26:57 +0200

    feat: Add listing create and edit routes
    Route /wystaw, store, edit and update, ordered so they do not clash
    with the show route.

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,12 @@ Route::prefix('panel')->name('panel.')->group(function () {
     Route::get('/wiadomosci', [DashboardController::class, 'messages'])->name('messages');
     Route::get('/konto', [DashboardController::class, 'account'])->name('account');
 });
+Route::get('/wystaw', [ListingController::class, 'create'])->name('listings.create');
+
 Route::get('/ogloszenia', [ListingController::class, 'index'])->name('listings.index');
+Route::post('/ogloszenia', [ListingController::class, 'store'])->name('listings.store');
+Route::get('/ogloszenia/{listing}/edytuj', [ListingController::class, 'edit'])->name('listings.edit');
+Route::put('/ogloszenia/{listing}', [ListingController::class, 'update'])->name('listings.update');
 Route::get('/ogloszenia/{listing}', [ListingController::class, 'show'])->name('listings.show');
 
 Route::get('/design-cheatsheet', function () {

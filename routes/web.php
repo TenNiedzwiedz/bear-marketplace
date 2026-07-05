2c6cafa2ffa33372f0fe47c4145c672be84eaf1c
TenNiedzwiedz <krawczyk_wojciech@wp.pl>
2026-07-05 12:12:31 +0200

    feat: Add /panel section routes
    Group the panel overview, listings, messages and account routes.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,19 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ListingController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', HomeController::class);
+
+Route::prefix('panel')->name('panel.')->group(function () {
+    Route::get('/', [DashboardController::class, 'overview'])->name('overview');
+    Route::get('/ogloszenia', [DashboardController::class, 'listings'])->name('listings');
+    Route::get('/wiadomosci', [DashboardController::class, 'messages'])->name('messages');
+    Route::get('/konto', [DashboardController::class, 'account'])->name('account');
+});
 Route::get('/ogloszenia', [ListingController::class, 'index'])->name('listings.index');
 Route::get('/ogloszenia/{listing}', [ListingController::class, 'show'])->name('listings.show');
 
